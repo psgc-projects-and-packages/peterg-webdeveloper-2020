0e21e18119294ceb905d5392a2a65e8abd0a8873
Peter Gorgone <peter@peltronic.com>
2020-04-16 11:01:37 -0700

    [INIT] -- use slug for show etc routes for project resource

--- psgcwd_server/app/controllers/projects_controller.rb
+++ psgcwd_server/app/controllers/projects_controller.rb
@@ -64,7 +64,8 @@ class ProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+      #@project = Project.find(params[:id])
+      @project = Project.find_by(slug: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
