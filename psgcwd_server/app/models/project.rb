78a1dd984c972c6be8ac67b0501cca01fbd88dc6
Peter Gorgone <peter@peltronic.com>
2020-04-15 17:06:11 -0700

    [INIT] -- impl of sluggable concern supporting multiple target fields

--- psgcwd_server/app/models/project.rb
+++ psgcwd_server/app/models/project.rb
@@ -6,6 +6,8 @@ class Project < ApplicationRecord
   belongs_to :enddate, class_name: :Monthperiod
 
   Sluggable.set_target_fields :ptitle
+  #Sluggable.set_target_fields :ptitle, :client
+
 #  before_create do
 #    self.slug = ptitle.parameterize
 #  end
--- psgcwd_server/app/models/sluggable.rb
+++ psgcwd_server/app/models/sluggable.rb
@@ -3,20 +3,22 @@ module Sluggable
 
   @@target_fields = []
 
-  def self.set_target_fields field1 
-    @@target_fields << field1
-  end
-
   included do
     before_create :slugify
   end
 
   def slugify
-    #byebug
-    self.slug = self[@@target_fields[0]].parameterize
+    str = @@target_fields.reduce('') { |a, s| a + "-" + self[s] } 
+    self.slug = str.parameterize
   end
+
+  def self.set_target_fields *fields 
+    @@target_fields = fields
+  end
+
 end
 
+# [ ] %TODO - write unit test
 # [ ] %TODO https://stackoverflow.com/questions/45445744/how-to-create-a-common-after-create-callback-for-all-models
 
 # move to concern? - https://stackoverflow.com/questions/46422576/how-do-i-reference-model-attributes-in-module-instance-methods-in-ror-5
